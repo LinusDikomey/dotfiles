599e9b437004a4bd97814874a069011320ba793d
Linus Dikomey <l.dikomey03@gmail.com>
2023-10-19 23:45:31 +0200

    add note and todo aliases for rsynced notes

---
+++ fish/functions/note.fish
@@ -0,0 +1,4 @@
+function note --wraps='~/Rsync/notes/edit-daily-note' --description 'alias note=~/Rsync/notes/edit-daily-note'
+  ~/Rsync/notes/edit-daily-note $argv
+        
+end
---
+++ fish/functions/todo.fish
@@ -0,0 +1,4 @@
+function todo --wraps='hx ~/Rsync/notes/todo.md' --description 'alias todo=hx ~/Rsync/notes/todo.md'
+  hx ~/Rsync/notes/todo.md $argv
+        
+end
