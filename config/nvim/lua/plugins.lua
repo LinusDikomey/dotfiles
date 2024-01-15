bee8994e597508fd98cab8b540a2cdd3cec33453
Linus Dikomey <linus.dikomey@rwth-aachen.de>
2024-01-15 19:43:13 +0100

    shorter nvim mode

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -107,7 +107,13 @@ return {
         section_separators = '',
       },
       sections = {
-        lualine_a = {'mode'},
+        lualine_a = {
+          { 'mode',
+            fmt = function(str)
+              return str:sub(1,3)
+            end
+          }
+        },
         lualine_b = {'branch', 'diff', 'diagnostics'},
         lualine_c = {'filename'},
         lualine_x = {'filetype'},
