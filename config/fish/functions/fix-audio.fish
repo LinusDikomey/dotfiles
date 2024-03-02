6df405df663dffb08b7f54ead40aa48deeccc7f8
Linus Dikomey <l.dikomey03@gmail.com>
2024-03-02 14:30:14 +0100

    clean up, install more via pacman instead of cargo

--- config/fish/functions/fix-audio.fish
+++
@@ -1,4 +0,0 @@
-function fix-audio --wraps='killall pulseaudio ; pulseaudio & ; disown' --wraps='killall pulseaudio && pulseaudio & && disown' --description 'alias fix-audio killall pulseaudio ; pulseaudio & ; disown'
-  killall pulseaudio ; pulseaudio & ; disown $argv
-        
-end
--- config/fish/functions/glb-json.fish
+++
@@ -1,4 +0,0 @@
-function glb-json --wraps=/home/linus/dotfiles/scripts/glb-json --wraps=/home/linus/scripts/glb-json --description 'alias glb-json /home/linus/scripts/glb-json'
-  /home/linus/scripts/glb-json $argv
-        
-end
