4df6f0c26795ce70133a287d60680259741002ee
Linus Dikomey <l.dikomey03@gmail.com>
2025-11-07 15:10:30 +0100

    add rust-overlay for rust toolchain, refactor packages, tweaks

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -7,43 +7,43 @@
 }: {
   options.dotfiles.coding.enable = lib.mkEnableOption "Enable coding packages";
 
-  config.home.packages = with pkgs; (
-    [
-      neovim
-      inputs'.agenix.packages.default
-      wget
-      ripgrep
-      bat
-      btop
-      imagemagick
-      zip
-      unzip
-      killall
-      tmux
-      dig
-    ]
-    ++ lib.optionals config.dotfiles.coding.enable [
-      clang
-      llvmPackages_19.clang-tools
-      lldb_19
-      cargo
-      rustc
-      clippy
-      rustfmt
-      rust-analyzer
-      inkscape
-      nixd
-      nil
-      inputs'.eye.packages.default
-    ]
-    ++ lib.optionals pkgs.stdenv.isDarwin [
-      vlc-bin
-    ]
-    ++ lib.optionals (pkgs.stdenv.isLinux && config.dotfiles.coding.enable) [
-      wineWowPackages.stable
-    ]
-  );
-  config.home.shellAliases = lib.mkIf config.dotfiles.coding.enable {
-    "objdump" = "objdump -M intel";
+  config = {
+    home.packages = with pkgs; (
+      [
+        neovim
+        inputs'.agenix.packages.default
+        wget
+        ripgrep
+        bat
+        btop
+        imagemagick
+        zip
+        unzip
+        killall
+        tmux
+        dig
+      ]
+      ++ lib.optionals config.dotfiles.coding.enable [
+        clang
+        llvmPackages_19.clang-tools
+        lldb_19
+        inkscape
+        nixd
+        nil
+        inputs'.eye.packages.default
+        (rust-bin.stable.latest.default.override {
+          extensions = ["rust-analyzer" "rust-src"];
+        })
+      ]
+      ++ lib.optionals pkgs.stdenv.isDarwin [
+        vlc-bin
+      ]
+      ++ lib.optionals (pkgs.stdenv.isLinux && config.dotfiles.coding.enable) [
+        wineWowPackages.stable
+      ]
+    );
+    home.shellAliases = lib.mkIf config.dotfiles.coding.enable {
+      "objdump" = "objdump -M intel";
+    };
   };
 }
